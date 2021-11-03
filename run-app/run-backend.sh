b94dba2dd5103acc4fc57a68f162efba3001cb5d
Mariana Betancur Florez <51976569+marianabetancur18@users.noreply.github.com>
2021-11-03 17:37:43 -0500

    BackEnd Run

--- run-app/run-backend.sh
+++ run-app/run-backend.sh
@@ -1,2 +1,11 @@
 #!/usr/bin/env bash
-echo "Hello from backend!"
+#Backend run
+#Part 1: Search for the binary
+cd /shared
+
+#Part 2: Search how to run the command in the background (aka, detach it from the terminal) so that you can close your terminal and the process will still runs in the background. Also, remember about the environment variable. It's a requirement for this exercise to run the app in the port 4001
+sudo echo 'export PORT=4001' >> /etc/profile
+
+#Part 3: Run the command in the background
+cd /shared/
+nohup ./vuego-demoapp > server.out 2>&1 &
